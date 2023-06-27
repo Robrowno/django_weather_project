c26ae1ae501168ef6db6ac6d285950b128c9d0ca
Christian Brown <robrowno@icloud.com>
2023-06-28 00:08:19 +0100

    create html pages to render basic information from openweather without any styling

--- django_weather_project/weatherApp/views.py
+++ django_weather_project/weatherApp/views.py
@@ -7,17 +7,17 @@ def home(request):
     """Render home.html template"""
 
     # Get API key from openweathermap.org
-    API_KEY = open("API_KEY", "r").read()
+    API_KEY = open("API_KEY", "r").read().strip()
     current_weather_url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}"
-    forcast_weather_url = "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&units=metric&appid={}&exclude=current,minutely,hourly,alerts"
+    forecast_weather_url = "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&units=metric&appid={}&exclude=current,minutely,hourly,alerts"
 
     if request.method == 'POST':
         city1 = request.POST['city1']
-        city2 = request.get('city2', None)
+        city2 = request.POST.get('city2', None)
 
-        weather_data1, daily_forecast1 = fetch_weather_and_forecast(city1, API_KEY, current_weather_url, forcast_weather_url)
+        weather_data1, daily_forecast1 = fetch_weather_and_forecast(city1, API_KEY, current_weather_url, forecast_weather_url)
         if city2:
-            weather_data2, daily_forecast2 = fetch_weather_and_forecast(city2, API_KEY, current_weather_url, forcast_weather_url)
+            weather_data2, daily_forecast2 = fetch_weather_and_forecast(city2, API_KEY, current_weather_url, forecast_weather_url)
         else:
             weather_data2, daily_forecast2 = None, None
         
@@ -31,17 +31,18 @@ def home(request):
     else:
         return render(request, 'weatherApp/home.html')
     
-def fetch_weather_and_forecast(city, api_key, current_weather_url, forcast_weather_url):
-    """Fetch weather and forecast data from openweathermap.org"""
+def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_weather_url):
+    current_weather_response = requests.get(current_weather_url.format(city, api_key))
+    current_weather_response.raise_for_status()
+    current_weather = current_weather_response.json()
 
-    # Fetch current weather data
-    current_weather = requests.get(current_weather_url.format(city, api_key)).json()
-    # Fetch forecast weather data
-    forecast_weather = requests.get(forcast_weather_url.format(current_weather['coord']['lat'], current_weather['coord']['lon'], api_key)).json()
+    forecast_response = requests.get(forecast_weather_url.format(current_weather['coord']['lat'], current_weather['coord']['lon'], api_key))
+    forecast_response.raise_for_status()
+    forecast_weather = forecast_response.json()
 
     weather_data = {
         'city': city,
-        'temperature': round(current_weather['main']['temp'] - 273.15, 2),
+        'temperature': round(current_weather['main']['temp'], 2),
         'description': current_weather['weather'][0]['description'],
         'icon': current_weather['weather'][0]['icon'],
         'humidity': current_weather['main']['humidity'],
@@ -55,7 +56,7 @@ def fetch_weather_and_forecast(city, api_key, current_weather_url, forcast_weath
     for data in forecast_weather['daily'][:5]:
         daily_forecast.append({
             'date': datetime.datetime.fromtimestamp(data['dt']).strftime('%d-%m-%Y'),
-            'temperature': round(data['temp']['day'] - 273.15, 2),
+            'temperature': round(data['temp']['day'], 2),
             'description': data['weather'][0]['description'],
             'icon': data['weather'][0]['icon'],
             'humidity': data['humidity'],
@@ -63,8 +64,4 @@ def fetch_weather_and_forecast(city, api_key, current_weather_url, forcast_weath
             'pressure': data['pressure'],
         })
 
-
     return weather_data, daily_forecast
-
-
-
