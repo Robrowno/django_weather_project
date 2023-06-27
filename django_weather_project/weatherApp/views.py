52665aa7733f55eaae7d0f5c81115f18623f45fd
Christian Brown <robrowno@icloud.com>
2023-06-27 17:32:39 +0100

    write home and featch weather functions in weatherAPP/views.py utilising openweather API

--- django_weather_project/weatherApp/views.py
+++ django_weather_project/weatherApp/views.py
@@ -1,9 +1,56 @@
+import datetime
+import requests
 from django.shortcuts import render
 
 
 def home(request):
     """Render home.html template"""
-    context = {}
-    return render(request, 'weatherApp/home.html', context)
+
+    # Get API key from openweathermap.org
+    API_KEY = open("API_KEY", "r").read()
+    current_weather_url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}"
+    forcast_weather_url = "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&units=metric&appid={}&exclude=current,minutely,hourly,alerts"
+
+    if request.method == 'POST':
+        city1 = request.POST['city1']
+        city2 = request.get('city2', None)
+    else:
+        return render(request, 'weatherApp/home.html')
+    
+def fetch_weather_and_forecast(city, api_key, current_weather_url, forcast_weather_url):
+    """Fetch weather and forecast data from openweathermap.org"""
+
+    # Fetch current weather data
+    current_weather = requests.get(current_weather_url.format(city, api_key)).json()
+    # Fetch forecast weather data
+    forecast_weather = requests.get(forcast_weather_url.format(current_weather['coord']['lat'], current_weather['coord']['lon'], api_key)).json()
+
+    weather_data = {
+        'city': city,
+        'temperature': round(current_weather['main']['temp'] - 273.15, 2),
+        'description': current_weather['weather'][0]['description'],
+        'icon': current_weather['weather'][0]['icon'],
+        'humidity': current_weather['main']['humidity'],
+        'wind_speed': current_weather['wind']['speed'],
+        'pressure': current_weather['main']['pressure'],
+        'sunrise': datetime.datetime.fromtimestamp(current_weather['sys']['sunrise']).strftime('%H:%M:%S'),
+        'sunset': datetime.datetime.fromtimestamp(current_weather['sys']['sunset']).strftime('%H:%M:%S'),
+    }
+
+    daily_forecast = []
+    for data in forecast_weather['daily'][:5]:
+        daily_forecast.append({
+            'date': datetime.datetime.fromtimestamp(data['dt']).strftime('%d-%m-%Y'),
+            'temperature': round(data['temp']['day'] - 273.15, 2),
+            'description': data['weather'][0]['description'],
+            'icon': data['weather'][0]['icon'],
+            'humidity': data['humidity'],
+            'wind_speed': data['wind_speed'],
+            'pressure': data['pressure'],
+        })
+
+
+    return weather_data, daily_forecast
+
 
 
